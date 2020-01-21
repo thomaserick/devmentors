1ee2346fbb02dbbef4f9c27b54057ac0bdbd1f35
Thomas <thominhaserick@gmail.com>
2020-01-21 18:14:09 -0300

    ajustando desafio-07

--- 03/exemple-app/src/desafio-07/user-new.js
+++ 03/exemple-app/src/desafio-07/user-new.js
@@ -3,13 +3,15 @@ import { LitElement, html, css } from "lit-element";
 class UserNew extends LitElement {
   constructor() {
     super();
-    this.todos = [];
-    this.name = "";
-    this.lastname = "";
-    this.email = "";
-    this.passwd = "";
-    this.confPasswd = "";
-    this.termos = false;
+
+    this.newUser = {
+      name: "",
+      lastname: "",
+      email: "",
+      passwd: "",
+      confPasswd: "",
+      termos: false
+    };
   }
 
   static get styles() {
@@ -18,7 +20,7 @@ class UserNew extends LitElement {
         display: block;
       }
 
-      .form-control {
+      input {
         border: 2px solid #7a7b7c;
         padding: 7px;
         width: 100%;
@@ -26,101 +28,88 @@ class UserNew extends LitElement {
         transition: border-color ease 0.15s;
       }
 
-      .form-control.focus {
-        outline: none !important;
-        border-color: #344ec3;
+      button {
+        background-color: green;
+        padding: 15px 25px;
+        border: none;
+        color: white;
+        font-size: 15px;
+        border-radius: 6px;
       }
     `;
   }
 
   static get properties() {
     return {
-      name: { type: String },
-      lasname: { type: String },
-      email: { type: String },
-      passwd: { type: String },
-      confPasswd: { type: String },
-      termos: { type: Boolean },
-      todos: { type: Array }
+      value: { type: String }
     };
   }
 
   render() {
     return html`
-  
-    <input type="text" id="name" placeholder="Nome" ></input></br></br>
-    <input type="text" id="lastname" placeholder="Sobrenome" /></br></br>
-    <input type="text" id="email" placeholder="e-mail" /></br></br>
-    <input type="text" id="passwd" placeholder="senha" /></br></br>
-    <input type="text" id="confPasswd" placeholder="Confirma Senha"/></br></br>
-    <input type="checkbox" ?checked=${this.termos}  @change=${this.change} />aceitar termos de condições.
-      </br></br>  
-      <button @click="${this.addUser}" >Confirmar</button>
+    <input type="text" id="name" placeholder="Nome"  @change="${this.changeName}" ></input></br></br>
+    <input type="text" id="lastname" placeholder="Sobrenome" @change="${this.changeLastName}"  /></br></br>
+    <input type="text" id="email" placeholder="e-mail" @change="${this.changeEmail}" /></br></br>
+    <input type="text" id="passwd" placeholder="senha" @change="${this.changePasswd}"  /></br></br>
+    <input type="text" id="confPasswd" placeholder="Confirma Senha" @change="${this.changeConfPasswd}" /></br></br>
+    <input type="checkbox" ?checked=${this.termos}  @change=${this.changeTermos}/>aceitar termos de condições 
+    </br></br>  
+    <button @click="${this.addUser}" >Confirmar</button>
 
     `;
   }
 
-  addUser() {
-    console.log("Nome" + this.name);
-    let inputName = this.shadowRoot.querySelector("#name");
-    let inputLastName = this.shadowRoot.querySelector("#lastname");
-    let inputEmail = this.shadowRoot.querySelector("#email");
-    let inputPasswd = this.shadowRoot.querySelector("#passwd");
-    let inputConfPasswd = this.shadowRoot.querySelector("#confPasswd");
-
-    this.name = inputName.value;
-    this.lastname = inputLastName.value;
-    this.email = inputEmail.value;
-    this.passwd = inputPasswd.value;
-    this.confPasswd = inputConfPasswd.value;
-
-    if (this.name === "") {
-      alert("Preencha o campo Nome");
-      inputName.focus();
-      return false;
-    }
-    if (this.lastname === "") {
-      alert("Preencha o campo Sobrenome");
-      inputLastName.focus();
-      return false;
-    }
-    if (this.email === "") {
-      alert("Preencha o campo Email");
-      inputEmail.focus();
-      return false;
-    }
-    if (this.passwd) {
-      alert("Preencha o campo Senha");
-      inputPasswd.focus();
-      return false;
-    }
-    if (this.confPasswd === "") {
-      alert("Preencha o campo Confirmar Senha");
-      inputConfPasswd.focus();
-      return false;
-    }
-    if (this.senha !== this.confPasswd) {
-      alert("A senha não Confere!");
-      inputPasswd.focus();
-      return false;
-    }
-
-    this.todos = [
-      {
-        name: this.name,
-        lastname: this.lastname,
-        email: this.email,
-        passwd: this.passwd,
-        confPasswd: this.confPasswd,
-        termos: this.termos
-      }
-    ];
-
-    console.log(this.todos);
+  changeName(e) {
+    this.newUser.name = e.target.value;
+  }
+  changeLastName(e) {
+    this.newUser.lastname = e.target.value;
+  }
+  changeEmail(e) {
+    this.newUser.email = e.target.value;
+  }
+  changePasswd(e) {
+    this.newUser.passwd = e.target.value;
+  }
+  changeConfPasswd(e) {
+    this.newUser.confPasswd = e.target.value;
   }
 
-  change(e) {
+  changeTermos(e) {
     this.termos = e.target.checked;
+  }
+
+  addUser() {
+    //let valid = true;
+    //let message = null;
+
+    if (this.newUser.name === "") {
+      alert("Preencha o campo Nome");
+      return false;
+    }
+
+    if (this.newUser.lastname === "") {
+      alert("Preencha o campo Sobrenome");
+      return false;
+    }
+    if (this.newUser.email === "") {
+      alert("Preencha o campo Email");
+      return false;
+    }
+    if (this.newUser.passwd) {
+      alert("Preencha o campo Senha");
+      return false;
+    }
+    if (this.newUser.confPasswd === "") {
+      alert("Preencha o campo Confirmar Senha");
+      return false;
+    }
+    if (this.newUser.senha !== this.confPasswd) {
+      alert("A senha não Confere!");
+      return false;
+    }
+
+    console.log(this.newUser);
   }
 
   updated(changedProperties) {
